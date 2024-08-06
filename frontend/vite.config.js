e717f8c9e36e2c60a006de35696bc3ad0190351e
rajankit1812 <rajankit120701@gmail.com>
2024-08-06 13:39:25 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy: {
-      '/api': 'http://localhost:3000'      //url request ka origin yhi hai to vite application khi pe bhi chlrhi ho pr proxy add krne se server ko lgega ki request aarhi hai 3000se jbki chlto rha tha vite app 5173 pe
+      '/api': 'https://jokes-p1rw.onrender.com'      //url request ka origin yhi hai to vite application khi pe bhi chlrhi ho pr proxy add krne se server ko lgega ki request aarhi hai 3000se jbki chlto rha tha vite app 5173 pe
     },
   },
   plugins: [react()],
